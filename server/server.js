41fc6713a5e84b5be89a4a9036a608effd949695
Shubham kumar <shubham210804@gmail.com>
2026-07-09 17:35:47 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+      "http://localhost:5173",
+      "https://your-frontend-name.onrender.com",
+    ],
+    credentials: true,
+  }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
